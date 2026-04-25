4456afa3c1f5ab30be43fff6216be232682b5868
safietoundiaye18 <safietoundiaye18@gmail.com>
2026-04-25 07:34:41 +0000

    Modification

--- routes/hotel.routes.js
+++ routes/hotel.routes.js
@@ -7,51 +7,28 @@ const {
   modifierHotel,
   supprimerHotel
 } = require('../controllers/hotel.controller');
-const { proteger } = require('../middleware/auth');
+const { proteger, autoriser } = require('../middleware/auth');
 const upload = require('../middleware/upload');
 
 // Routes publiques
 router.get('/', obtenirHotels);
 router.get('/:id', obtenirHotel);
 
-// Routes protégées
-router.post('/', proteger, (req, res, next) => {
+// Routes admin seulement
+router.post('/', proteger, autoriser('admin'), (req, res, next) => {
   upload.single('image')(req, res, (err) => {
-    if (err) {
-      return res.status(400).json({
-        succes: false,
-        message: err.message
-      });
-    }
+    if (err) return res.status(400).json({ succes: false, message: err.message });
     next();
   });
 }, creerHotel);
 
-router.put('/:id', proteger, (req, res, next) => {
+router.put('/:id', proteger, autoriser('admin'), (req, res, next) => {
   upload.single('image')(req, res, (err) => {
-    if (err) {
-      return res.status(400).json({
-        succes: false,
-        message: err.message
-      });
-    }
+    if (err) return res.status(400).json({ succes: false, message: err.message });
     next();
   });
 }, modifierHotel);
 
-router.delete('/:id', proteger, supprimerHotel);
+router.delete('/:id', proteger, autoriser('admin'), supprimerHotel);
 
 module.exports = router;
-
-/*
-
-
-Explication :
-
-router.get('/') → publique, tout le monde peut voir les hôtels sans être connecté
-router.get('/:id') → publique, tout le monde peut voir un hôtel
-proteger → pour créer, modifier ou supprimer, il faut être connecté
-upload.single('image') → Multer traite l'image avant que le controller s'exécute. single veut dire qu'on attend une seule image avec le champ nommé image
-/:id → le :id est une partie dynamique de l'URL. Par exemple /api/hotels/123 ou /api/hotels/abc
-
-*/
